302b11affe149a7b5201ac119ae618241c20e533
李佐群 <2645929971@qq.com>
2024-12-17 20:07:48 +0800

    init

---
+++ src/main/java/com/modify/ServiceHardwareApplication.java
@@ -0,0 +1,13 @@
+package com.modify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHardwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHardwareApplication.class, args);
+    }
+
+}
