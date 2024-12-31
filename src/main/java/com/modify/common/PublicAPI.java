b64416c4e654714498df406f436d5f250e77f31c
李佐群 <2645929971@qq.com>
2024-12-31 14:19:06 +0800

    feat(public-api): 添加公共 API接口和相关控制器
    - 新增 PublicAPI 类，定义公共 API 的基础 URL
    - 添加 AdviceController，实现建议搜索功能
    - 添加 AnimalController，实现动物相关 API接口
    - 添加 DateController，实现日期相关 API 接口- 更新 deviceController，移除跨域注解
    - 新增 CatType 实体类
    - 在 pom.xml 中添加 hutool-all依赖

---
+++ src/main/java/com/modify/common/PublicAPI.java
@@ -0,0 +1,8 @@
+package com.modify.common;
+
+/**
+ * @author modify
+ */
+public class PublicAPI {
+    public static final String API_URL = "https://api.jiejiariapi.com/v1/holidays/";
+}
--- src/main/java/com/modify/controller/deviceController.java
+++ src/main/java/com/modify/controller/deviceController.java
@@ -3,18 +3,18 @@ package com.modify.controller;
 import com.modify.common.R;
 import com.modify.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author modify
+ *
+ * OSHI 控制层
  */
 
 @RequestMapping("/device")
 @RestController
-@CrossOrigin
 public class deviceController {
 
     @Autowired
